5bc08cc7a22bde4a9eeab96d8bffb2c26dfc08a9
bad-ruby hub <nazim.fataliev@list.ru>
2024-11-04 22:32:55 +0400

    add button"add" in form db

--- components/Button/index.jsx
+++ components/Button/index.jsx
@@ -1,16 +1,45 @@
 import { useState } from "react";
 import classes from "./Button.module.css"
+import { ObjTable } from "../ObjTable";
 export function ButtonAdd({id}) {
-    return <form action="" method="get">
+    const [name, setName] = useState(""),
+          [email, setEmail] = useState(""),
+          [phone, setPhone] = useState("???"),
+          [address, setAddress] = useState({city: "???", street: "???", suite: "???"}),
+          [company, setCompany] = useState({name: "???"}),
+          [error, setError] = useState(null);
+    const add = async  ()=> {
+        try {
+            request = await fetch(`http://localhost:3001/users`,
+                {method: "POST", 
+                headers:{'Content-Type': 'application/json'},
+                body: JSON.stringify({name,email,phone,address,company}),
+            });
+            setName("");
+            setEmail("");
+            setPhone("???");
+            setAddress({city: "???", street: "???", suite: "???"});
+            setCompany({name: "???"});
+            console.log("Пользователь добавлен успешно!");
+
+        } catch (err) {
+            setError(err.message);
+        }
+    }  
+
+    return <form onSubmit={add}>
   <p>
     <label for="name">Name: </label>
-    <input placeholder="Введите имя" type="text" />
+    <input value={name}
+           onChange={(e) => setName(e.target.value)} placeholder="Введите имя" type="text" />
   </p>
   <p>
     <label for="email">Email: </label>
-    <input placeholder="Введите почту" type="email"/>
+    <input value={email}
+           onChange={(e) => setEmail(e.target.value)} placeholder="Введите почту" type="text"/>
   </p>
-  <button type="submit">добавить пользователя</button>
+
+  <button  type="submit">добавить пользователя</button>
 
 </form>
 
@@ -22,8 +51,9 @@ export function ButtonDel({id}) {
         try {
             request = await fetch(`http://localhost:3001/users/${id}`,{method: "DELETE"});
         } catch (err) {
-            setError(err)
+            setError(err.message);
         }
     }       
     return <button onClick={delet}>Удалить пользователя</button>
 }
+
